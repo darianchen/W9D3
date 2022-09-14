1298d8176375233ebc8a1cc09edf62f63e51a8f3
darianchen <darianychen@gmail.com>
2022-09-14 16:55:25 -0400

    change button text

--- app/javascript/follow_toggle.js
+++ app/javascript/follow_toggle.js
@@ -8,24 +8,45 @@ export default class FollowToggle {
 
   async handleClick(event) {
     event.preventDefault();
-    if (this.followState){
-      this.unfollow();
-    }else{
+    if (this.followState !== 'followed'){
       this.follow();
+    }else{
+      this.unfollow();
     }
   }
 
   async follow() {
-    
+    console.log(this.toggleButton.dataset.userId)
+    await API.followUser(this.toggleButton.dataset.userId);
+    this.followState = 'followed'
   }
 
   async unfollow() {
     // Your code here
+    console.log(this.toggleButton.dataset.userId)
+    await API.followUser(this.toggleButton.dataset.userId);
+    this.followState = 'unfollowed'
   }
 
   render() {
     switch (this.followState) {
-      // Your code here
+      case "followed":
+        this.toggleButton.disabled = false;
+        this.toggleButton.innerText = 'Unfollow!'
+        break;
+      case "unfollowed":
+        this.toggleButton.disabled = true;
+        this.toggleButton.innerText = 'Follow!!'
+        break;
+      case "following":
+        this.toggleButton.disabled = true;
+        this.toggleButton.innerText = 'Following...'
+        break;
+      case "following":
+        this.toggleButton.disabled = true;
+        this.toggleButton.innerText = 'Unfollowing...'
+        break;
+
     }
   }
 
--- app/javascript/util/api.js
+++ app/javascript/util/api.js
@@ -1,10 +1,31 @@
+// import API from './index'
 const csrfToken = document.querySelector("meta[name=csrf-token]").content;
+export const temp = "test";
+  export function followUser(id){
+    customFetch(`/users/${id}/follow`,{method: 'post'}
+    )
+  }
+
+  export function unfollowUser(id){
+    customFetch(`/users/${id}/follow`,{method: 'delete'})
+  }
 
 async function customFetch(url, options = {}) {
   options.headers = {
-    // Your code here
+    'Accept': 'application/json',
+    'X-CSRF-Token': csrfToken,
+
+
     ...options.headers
   };
 
-  return await fetch(url, options);
+  let response = await fetch(url, options);
+  if (response.ok === true){
+    return response.json();
+  }else{
+    throw response;
+  }
 }
+// export const followUser = followUser;
+// export const unfollowUser = unfollowUser;
+// window.API = API;
